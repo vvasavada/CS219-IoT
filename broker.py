9cc9c50d68621578400a31ef92d61c4a7c5f1e9b
Vishrant Vasavada <v.vasavada@hotmail.com>
2019-02-10 21:41:42 -0800

    clean broker code

--- broker.py
+++ broker.py
@@ -23,28 +23,6 @@ from hbmqtt.broker import Broker
 from hbmqtt.version import get_version
 from docopt import docopt
 from hbmqtt.utils import read_yaml_config
-
-
-default_config = {
-    'listeners': {
-        'default': {
-            'type': 'tcp',
-            'bind': '0.0.0.0:1883',
-        },
-    },
-    'sys_interval': 10,
-    'auth': {
-        'allow-anonymous': True,
-        'password-file': os.path.join(os.path.dirname(os.path.realpath(__file__)), "passwd"),
-        'plugins': [
-            'auth_file', 'auth_anonymous'
-        ]
-    },
-    'topic-check': {
-        'enabled': False
-    }
-}
-
 logger = logging.getLogger(__name__)
 
 
@@ -53,19 +31,10 @@ def main(*args, **kwargs):
         logger.fatal("Error: Python 3.4+ is required")
         sys.exit(-1)
 
-    #arguments = docopt(__doc__, version=get_version())
     formatter = "[%(asctime)s] :: %(levelname)s - %(message)s"
-
-    #if arguments['-d']:
     level = logging.DEBUG
-    #else:
-    #    level = logging.INFO
     logging.basicConfig(level=level, format=formatter)
 
-    config = None
-    #if arguments['-c']:
-    #    config = read_yaml_config(arguments['-c'])
-    #else:
     config = read_yaml_config(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'broker_config.yaml'))
     logger.debug("Using default configuration")
 
