81083f258e11f53a25d1635befbe45ecb49828d8
Vishrant Vasavada <v.vasavada@hotmail.com>
2019-02-22 10:41:41 -0800

    using different acl config file

---
+++ client_publish.py
@@ -0,0 +1,34 @@
+import logging
+import asyncio
+
+from hbmqtt.client import MQTTClient
+from hbmqtt.mqtt.constants import QOS_1, QOS_2
+
+logger = logging.getLogger(__name__)
+
+config = {
+    'will': {
+        'topic': '/will/client',
+        'message': b'Dead or alive',
+        'qos': 0x01,
+        'retain': True,
+    },
+}
+C = MQTTClient(config=config)
+
+@asyncio.coroutine
+def test_coro():
+    yield from C.connect('mqtts://test2:test@0.0.0.0:8883', cafile='ca.crt')
+    tasks = [
+        asyncio.ensure_future(C.publish('a/b', b'TEST MESSAGE WITH QOS_0')),
+    ]
+    yield from asyncio.wait(tasks)
+    logger.info("messages published")
+    yield from C.disconnect()
+
+
+if __name__ == '__main__':
+    formatter = "[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s"
+    logging.basicConfig(level=logging.DEBUG, format=formatter)
+    asyncio.get_event_loop().run_until_complete(test_coro())
+
--- hbmqtt/plugins/topic_checking.py
+++ hbmqtt/plugins/topic_checking.py
@@ -1,5 +1,5 @@
 import asyncio
-
+from hbmqtt.utils import read_yaml_config
 
 class BaseTopicPlugin:
     def __init__(self, context):
@@ -72,7 +72,12 @@ class TopicAccessControlListPlugin(BaseTopicPlugin):
                 username = session.username
                 if username is None:
                     username = 'anonymous'
-                allowed_topics = self.topic_config['acl'].get(username, None)
+                acl_file = self.topic_config['acl'].get('file', None)
+                if not acl_file:
+                    self.context.logger.warning("'file' acl parameter not found")
+                    return False
+                acl_configs = read_yaml_config(acl_file)
+                allowed_topics = acl_configs['acl'].get(username, None)
                 if allowed_topics:
                     for allowed_topic in allowed_topics:
                         if self.topic_ac(req_topic, allowed_topic):
