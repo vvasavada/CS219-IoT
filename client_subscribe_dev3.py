e337eee4885f4be23cd5dc5357c2d0982ab34066
Vishrant Vasavada <v.vasavada@hotmail.com>
2019-03-13 14:48:49 -0700

    adding demo files

---
+++ client_subscribe_dev3.py
@@ -0,0 +1,29 @@
+import logging
+import asyncio
+
+from hbmqtt.client import MQTTClient, ClientException
+from hbmqtt.mqtt.constants import QOS_1, QOS_2
+
+logger = logging.Logger("client_sub")
+
+@asyncio.coroutine
+def uptime_coro():
+    C = MQTTClient()
+    yield from C.connect('mqtts://vaibhavagg2-device3:password-device3@0.0.0.0:8883', cafile='ca.crt')
+    yield from C.subscribe([
+            ('vaibhavagg2/test', QOS_1),
+            ('vaibhavagg2/device2/test', QOS_1)
+         ])
+    try:
+        for i in range(1, 100):
+            message = yield from C.deliver_message()
+            packet = message.publish_packet
+            print("%d:  %s => %s" % (i, packet.variable_header.topic_name, str(packet.payload.data)))
+        yield from C.unsubscribe(['a/b', 'test_topic'])
+        yield from C.disconnect()
+    except ClientException as ce:
+        logger.error("Client exception: %s" % ce)
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(uptime_coro())
+
